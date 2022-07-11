2b2bae608c57408f6c9b733df547e1392e89593b
sretenovich <ivan.sretenovic@endava.com>
2022-07-11 22:27:33 +0200

    commit 3

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast")
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -70,8 +71,8 @@ app.get('*', (req, res) => {
     res.send("404 page")
 })
 
-app.listen(3000, () => {
-    console.log("Server is up and running on port: 3000.")
+app.listen(port, () => {
+    console.log("Server is up and running on port: " + port)
 })
 
 // const currentYear = new Date().getFullYear()
